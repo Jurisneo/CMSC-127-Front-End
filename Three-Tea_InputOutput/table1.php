b0c486e828f6869420bc493fb7267de990995c3e
Juris Neo Concepcion <100197523+Jurisneo@users.noreply.github.com>
2022-12-28 18:13:12 +0800

    Update table1.php

--- Three-Tea_InputOutput/table1.php
+++ Three-Tea_InputOutput/table1.php
@@ -1,40 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>CHECKLIST Table</title>
-<title>Table with database</title>
+    <head>
+        <title>COURSE Table</title>
         <link href="style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css"/>
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <title>BSC</title>
         <link rel="icon" href="imgs/BSC.png" type="image/ico" >
-</head>
-<body>
-<nav class="navBar">
-            <div class="navLogo">
-                <a href="">B<span>S</span>C</a>
-            </div>
-            <div class="navLinks">
-                <ul>
-                    <li><a href="filter.php">HOME</a></li>
-                    <li><a href="table1.php">CHECKLIST</a></li>
-                    <li><a href="table2.php">COURSE</a></li>
-                    <li><a href="table3.php">INSTRUCTOR</a></li>
-                    <li><a href="table4.php">STUDENT</a></li>
-                    <li><a href="table5.php">ROOM</a></li>
-                </ul>
-            </div>
-            <div class="navIcons">
-                <a href="filter.php"><i class='bx bx-search-alt' ></i></a>
-                <a href=""><i class='bx bx-log-out-circle' ></i></a>
-            </div>
-	<br>
-	<div>
-        </nav>
-    <body>
-    <table>
-    <tr>
-        <style>
-    table {
+<style>
+table {
 border-collapse: collapse;
 width: 100%;
 color: #588c7e;
@@ -48,15 +21,39 @@ color: white;
 }
 tr:nth-child() {background-color: #f2f2f2}
 </style>
-        <th>Checklist ID</th>
-        <th>Student ID</th>
-        <th>Course ID</th>
-        <th>Course Sem Taken</th>
-        <th>Academic Year</th>
+    </head>
+    <nav class="navBar">
+            <div class="navLogo">
+                <a href="">B<span>S</span>C</a>
+            </div>
+            <div class="navLinks">
+                <ul>
+                <li><a href="filter.php">HOME</a></li>
+                    <li><a href="table1.php">CHECKLIST</a></li>
+                    <li><a href="table2.php">COURSE</a></li>
+                    <li><a href="table3.php">INSTRUCTOR</a></li>
+                    <li><a href="table4.php">STUDENT</a></li>
+                    <li><a href="table5.php">ROOM</a></li>
+                </ul>
+            </div>
+            <div class="navIcons">
+                <a href="filter.php"><i class='bx bx-search-alt' ></i></a>
+                <a href=""><i class='bx bx-log-out-circle' ></i></a>
+            </div>
+        </nav>
+    <body>
+    <table>
+    <tr>
+    <th>Course ID</th>
+        <th>Course Category</th>
+        <th>Course Name</th>
+        <th>Description</th>
+        <th>Component</th>
+        <th>Course Semester</th>
+        <th>Instructor ID</th>
+        <th>Room ID</th> 
         <th>Action</th>
     </tr>
-    <?php
-    ?>
     <footer class="footer">
             <div class="footer1">
                 <div class="footerLogo">
@@ -75,36 +72,36 @@ tr:nth-child() {background-color: #f2f2f2}
                 </div>
             </div>
         </footer>
-			<tbody>
 
-            <?php
-            $conn = mysqli_connect("localhost", "root", "", "CMSC127_RSC_db");
-            // Check connection
-            if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-            }
-            $sql = "SELECT CList_ID, Stud_ID, Crs_ID, CList_SemTaken, CList_AcadYear FROM CHECKLIST";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo
-                "<tr>
-                <td>". $row["CList_ID"] ."</td>
-                <td>". $row["Stud_ID"] ."</td>
-                <td>". $row["Crs_ID"] ."</td>
-                <td>". $row["CList_SemTaken"] ."</td>
-                <td>". $row["CList_AcadYear"] ."</td>
-                <td><a  href='delete.php'? CList_ID=". 
+
+<?php
+$conn = mysqli_connect("localhost", "root", "", "CMSC127_RSC_db");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT Crs_ID, Crs_Categ, Crs_Name, Crs_Descript, Crs_Comp, Crs_Sem, Ins_ID, Rm_ID  FROM COURSE";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+    echo "<tr>
+    <td>" . $row["Crs_ID"]. "</td>
+    <td>" . $row["Crs_Categ"] ."</td>
+    <td>" .$row["Crs_Name"]."</td>
+    <td>". $row["Crs_Descript"]."</td>
+    <td>". $row["Crs_Comp"]."</td>
+    <td>". $row["Crs_Sem"]."</td>
+    <td>". $row["Ins_ID"]."</td>
+    <td>". $row["Rm_ID"]."</td>
+    <td><a href='delete1.php'? id=". 
                 $row["CList_ID"] .">DELETE</a></td>
-            </tr>";
-            }
-            echo "</table>";
-            } else { echo "0 results"; }
-            $conn->close();
-            ?>
-			</tbody>
-		</table>
-	</div>
+    </tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
 </body>
 </html>
