fe7b68ea35547cc9a90d3980ecf0c1c40079f1e8
Juris Neo Concepcion <100197523+Jurisneo@users.noreply.github.com>
2022-12-30 11:28:40 +0800

    Add files via upload

---
+++ Three-Tea/pages/homepage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="../style1.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css"/>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <title>BSC</title>
+        <link rel="icon" href="imgs/BSC.png" type="image/ico" >
+    </head>
+    <body>
+
+        <section class="fContainer1">
+            <div class="fTitle1">
+                <h1>UNIVERSITY OF THE PHILIPPINES BAGUIO</h1>
+                <p>Bachelor of Science in Computer Science
+                </p>
+                <button class="button button1" onclick="location.href='LS-Stud.php'">LOGIN AS STUDENT</button>
+                <button class="button button2" onclick="location.href='LS-Ad.php'">LOGIN AS ADMIN</button>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
